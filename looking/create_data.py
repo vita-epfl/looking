19f3404751db9550aebd094b9298308c6199f101
Romain Caristan <romain.caristan@epfl.ch>
2021-03-22 12:01:28 +0100

    Update create_data.py

--- looking/create_data.py
+++ looking/create_data.py
@@ -89,7 +89,7 @@ def crop_jaad(img, bbox):
 
 ## paths
 
-path_jaad = "../data/romaincaristan-data/JAAD/images/"
+path_jaad = "/data/romaincaristan-data/JAAD/images/"
 path_joints = "./data/output_pifpaf_01_2k30/"
 
 
