7420570e2fdffbc3f80c5c01264a338d597dfab7
younesbelkada <younesbelkada@gmail.com>
2021-05-03 10:44:37 +0200

    add jackrabbot

---
+++ looking_new/create_data/create_jr.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+BASE_PATH=$(pwd)
+PATH_OUT="/home/younesbelkada/Travail/data"
+ID_JR='16FoP4W2txjHAM9F4uoOJnjVxsIW1OsQ3'
+PATH_SPLITS="/home/younesbelkada/Travail/looking/looking_new/create_data/splits_jack"
+
+echo "Downloading JackRabbot dataset"
+
+mkdir -p "${PATH_OUT}/JackRabbot"
+
+gdown --id $ID_JR
+mv reviewed.zip "${PATH_OUT}/JackRabbot"
+
+cd "${PATH_OUT}/JackRabbot"
+unzip reviewed.zip
+
+cd $BASE_PATH
+
+python create_nu_jr.py --train_file trainval.txt --test_file test.txt --path_txt $PATH_SPLITS --path_out "${PATH_OUT}/JackRabbot"
