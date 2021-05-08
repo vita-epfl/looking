6300add68313c9bf83676cac2fd660b5a08fca06
younesbelkada <younesbelkada@gmail.com>
2021-05-08 02:26:02 +0200

    update gdrive link jackrabbot

--- looking_new/create_data/create_jr.sh
+++ looking_new/create_data/create_jr.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 BASE_PATH=$(pwd)
 PATH_OUT="/home/younesbelkada/Travail/data"
-ID_JR='16FoP4W2txjHAM9F4uoOJnjVxsIW1OsQ3'
+ID_JR='1hazKAEdWQjjwPwmksL8XbyT9W8A6zn0Z'
 PATH_SPLITS="/home/younesbelkada/Travail/looking/looking_new/create_data/splits_jack"
 
 echo "Downloading JackRabbot dataset"
 
 mkdir -p "${PATH_OUT}/JackRabbot"
 
-gdown --id $ID_JR
-mv reviewed.zip "${PATH_OUT}/JackRabbot"
+#gdown --id $ID_JR
+#mv reviewed.zip "${PATH_OUT}/JackRabbot"
 
-cd "${PATH_OUT}/JackRabbot"
-unzip reviewed.zip
+#cd "${PATH_OUT}/JackRabbot"
+#unzip reviewed.zip
 
 cd $BASE_PATH
 
