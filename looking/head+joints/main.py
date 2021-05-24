82fd871821efbe1978ccf9f9e6673e1b5b87f595
Romain Caristan <romaincaristan@yahoo.fr>
2021-05-25 01:30:55 +0200

    Correct indentation error

--- looking/head+joints/main.py
+++ looking/head+joints/main.py
@@ -56,54 +56,54 @@ print('Device: ', device)
 
 
 if model_type == "alexnet":
-	data_transform = transforms.Compose([
-		SquarePad(),
+    data_transform = transforms.Compose([
+        SquarePad(),
         transforms.ToTensor(),
-	transforms.ToPILImage(),
+    transforms.ToPILImage(),
         transforms.Resize((227,227)),
-	transforms.ToTensor(),
+    transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
-	])
-	net = models.alexnet(pretrained=True).to(device)
-	net.classifier  = nn.Sequential(
-            	nn.Dropout(),
-            	nn.Linear(256 * 6 * 6, 4096),
-            	nn.ReLU(inplace=True),
-            	nn.Dropout(),
-            	nn.Linear(4096, 4096),
-            	nn.ReLU(inplace=True),
-            	nn.Linear(4096, 1),
-        	    nn.Sigmoid()
-	).to(device)
-	for param in net.parameters():
-        	param.requires_grad = False
+    ])
+    net = models.alexnet(pretrained=True).to(device)
+    net.classifier  = nn.Sequential(
+                nn.Dropout(),
+                nn.Linear(256 * 6 * 6, 4096),
+                nn.ReLU(inplace=True),
+                nn.Dropout(),
+                nn.Linear(4096, 4096),
+                nn.ReLU(inplace=True),
+                nn.Linear(4096, 1),
+                nn.Sigmoid()
+    ).to(device)
+    for param in net.parameters():
+            param.requires_grad = False
 
 
-	for param in net.classifier.parameters():
-	        param.requires_grad = True
+    for param in net.classifier.parameters():
+            param.requires_grad = True
 else:
-	data_transform = transforms.Compose([
-		SquarePad(),
+    data_transform = transforms.Compose([
+        SquarePad(),
         transforms.ToTensor(),
-	transforms.ToPILImage(),
+    transforms.ToPILImage(),
         transforms.Resize((224,224)),
-	transforms.ToTensor(),
+    transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
-	])
-	if model_type == "resnet18":
-		net = models.resnet18(pretrained=True)
-		net.fc  = nn.Sequential(
-			nn.Linear(in_features=512, out_features=1, bias=True),
-			nn.Sigmoid()
-		).to(device)
-	elif model_type == "resnet50":
-		net = models.resnext50_32x4d(pretrained=True)
-		net.fc  = nn.Sequential(
-			nn.Linear(in_features=2048, out_features=1, bias=True),
-			nn.Sigmoid()
-		).to(device)
+    ])
+    if model_type == "resnet18":
+        net = models.resnet18(pretrained=True)
+        net.fc  = nn.Sequential(
+            nn.Linear(in_features=512, out_features=1, bias=True),
+            nn.Sigmoid()
+        ).to(device)
+    elif model_type == "resnet50":
+        net = models.resnext50_32x4d(pretrained=True)
+        net.fc  = nn.Sequential(
+            nn.Linear(in_features=2048, out_features=1, bias=True),
+            nn.Sigmoid()
+        ).to(device)
 
 print("model type {} | split type : {}".format(model_type, split))
 
@@ -135,77 +135,77 @@ losses = []
 ap_max = 0
 
 for e in range(EPOCHS):
-	for heads, keypoints, y_batch in dataset_loader:
-		if use_cuda:
-			heads, keypoints, y_batch = heads.to(device), keypoints.to(device), y_batch.to(device)
-		optimizer.zero_grad()
-		output = model(heads, keypoints)
-		l = loss(output.view(-1), y_batch.type(torch.float).view(-1)).to(device)
-		l.backward()
-		optimizer.step()
-		i += 1
-		model.eval()
-		pred_label = torch.round(output)
-		accs.append(binary_acc(pred_label.type(torch.float).view(-1), y_batch).item())
-		losses.append(l.item())
-		model.train()
-		if i%10 == 0:
-			print('step {} , m_loss :{} | m_acc:{} '.format(i, np.mean(losses), np.mean(accs)))
-			accs = []
-			losses = []
-	model.eval()
-	torch.cuda.empty_cache()
-	acc = 0
-	ap = 0
-	out_lab = torch.Tensor([]).type(torch.float)
-	test_lab = torch.Tensor([])
+    for heads, keypoints, y_batch in dataset_loader:
+        if use_cuda:
+            heads, keypoints, y_batch = heads.to(device), keypoints.to(device), y_batch.to(device)
+        optimizer.zero_grad()
+        output = model(heads, keypoints)
+        l = loss(output.view(-1), y_batch.type(torch.float).view(-1)).to(device)
+        l.backward()
+        optimizer.step()
+        i += 1
+        model.eval()
+        pred_label = torch.round(output)
+        accs.append(binary_acc(pred_label.type(torch.float).view(-1), y_batch).item())
+        losses.append(l.item())
+        model.train()
+        if i%10 == 0:
+            print('step {} , m_loss :{} | m_acc:{} '.format(i, np.mean(losses), np.mean(accs)))
+            accs = []
+            losses = []
+    model.eval()
+    torch.cuda.empty_cache()
+    acc = 0
+    ap = 0
+    out_lab = torch.Tensor([]).type(torch.float)
+    test_lab = torch.Tensor([])
 
-	ap, acc = jaad_val.evaluate(model, device, 1)
-	print('epoch {} | acc:{} | ap:{}'.format(e+1, acc,ap))
+    ap, acc = jaad_val.evaluate(model, device, 1)
+    print('epoch {} | acc:{} | ap:{}'.format(e+1, acc,ap))
 
 
-	if ap > ap_max:
-		ap_max = ap
-		torch.save(model.state_dict(), PATH_MODEL + "model_combined_{}_{}_new_crops.p".format(model_type, video))
-	model.train()
+    if ap > ap_max:
+        ap_max = ap
+        torch.save(model.state_dict(), PATH_MODEL + "model_combined_{}_{}_new_crops.p".format(model_type, video))
+    model.train()
 
 data_test_jaad = JAAD_Dataset(DATA_PATH, JAAD_PATH,"test", SPLIT_PATH, split, data_transform)
 ap, acc = data_test_jaad.evaluate(model, device, 10)
 print('Performance on JAAD | acc:{} | ap:{}'.format(acc,ap))
 
 if kitti:
-	#model = []
-	#model = torch.load(PATH_MODEL + "model_combined_{}_{}_new_crops.p".format(video, pose), map_location=torch.device(device))
-	jaad_val = Kitti_Dataset(DATA_PATH, "test", pose)
+    #model = []
+    #model = torch.load(PATH_MODEL + "model_combined_{}_{}_new_crops.p".format(video, pose), map_location=torch.device(device))
+    jaad_val = Kitti_Dataset(DATA_PATH, "test", pose)
 
 
-	joints_test, labels_test = jaad_val.get_joints()
+    joints_test, labels_test = jaad_val.get_joints()
 
-	out_test = model(joints_test.to(device))
-	acc_test = binary_acc(out_test.to(device), labels_test.view(-1,1).to(device))
-	ap_test = average_precision(out_test.to(device), labels_test.to(device))
+    out_test = model(joints_test.to(device))
+    acc_test = binary_acc(out_test.to(device), labels_test.view(-1,1).to(device))
+    ap_test = average_precision(out_test.to(device), labels_test.to(device))
 
-	print("Kitti | AP : {} | Acc : {}".format(ap_test, acc_test))
+    print("Kitti | AP : {} | Acc : {}".format(ap_test, acc_test))
 
 
-	data_test= Kitti_Dataset(DATA_PATH, "test", pose)
-	dataset_loader_test = torch.utils.data.DataLoader(data_test,batch_size=8, shuffle=True)
-	model.eval()
-	acc = 0
-	ap = 0
+    data_test= Kitti_Dataset(DATA_PATH, "test", pose)
+    dataset_loader_test = torch.utils.data.DataLoader(data_test,batch_size=8, shuffle=True)
+    model.eval()
+    acc = 0
+    ap = 0
 
-	out_lab = torch.Tensor([]).type(torch.float).to(device)
-	test_lab = torch.Tensor([]).to(device)
-	for heads, keypoints, y_test in dataset_loader_test:
-	    if use_cuda:
-	        heads, keypoints, y_test = heads.cuda(), keypoints.cuda(), y_test.cuda()
-	    output = model(heads, keypoints)
-	    out_pred = output
-	    le = heads.shape[0]
-	    pred_label = torch.round(out_pred)
-	    test_lab = torch.cat((test_lab.detach().cpu(), y_test.detach().cpu().view(-1)), dim=0)
-	    out_lab = torch.cat((out_lab.detach().cpu(), out_pred.view(-1).detach().cpu()), dim=0)
+    out_lab = torch.Tensor([]).type(torch.float).to(device)
+    test_lab = torch.Tensor([]).to(device)
+    for heads, keypoints, y_test in dataset_loader_test:
+        if use_cuda:
+            heads, keypoints, y_test = heads.cuda(), keypoints.cuda(), y_test.cuda()
+        output = model(heads, keypoints)
+        out_pred = output
+        le = heads.shape[0]
+        pred_label = torch.round(out_pred)
+        test_lab = torch.cat((test_lab.detach().cpu(), y_test.detach().cpu().view(-1)), dim=0)
+        out_lab = torch.cat((out_lab.detach().cpu(), out_pred.view(-1).detach().cpu()), dim=0)
 
-	ap = average_precision(out_lab, test_lab)
-	acc = binary_acc(torch.round(out_lab).type(torch.float).view(-1), test_lab).item()
-	print('Performance on Kitti | acc:{} | ap:{}'.format(acc,ap))
+    ap = average_precision(out_lab, test_lab)
+    acc = binary_acc(torch.round(out_lab).type(torch.float).view(-1), test_lab).item()
+    print('Performance on Kitti | acc:{} | ap:{}'.format(acc,ap))
