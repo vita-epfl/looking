0fbe0ac083ef7250465a8c37058ca9aee3e3d1e0
Romain Caristan <romaincaristan@yahoo.fr>
2021-05-31 02:15:11 +0200

    Eyes folder

---
+++ looking/eyes/dataset.py
@@ -0,0 +1,158 @@
+from __future__ import print_function, division
+import os
+import torch
+from skimage import io, transform
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+import json
+from glob import glob
+from PIL import Image
+from utils import *
+
+np.random.seed(0)
+
+
+class JAAD_Dataset(Dataset):
+	"""JAAD dataset for training and inference"""
+
+	def __init__(self, path, path_jaad, split, split_path, transform=None):
+		"""
+		Args:
+			split : train, val and test
+			type_ : type of dataset splitting (original splitting, video splitting, pedestrian splitting)
+			transform : data tranformation to be applied
+		"""
+		self.data = None
+		self.path = path
+		self.path_jaad = path_jaad
+		self.split_path = split_path
+		self.split = split
+
+
+		self.txt = open(self.split_path + 'jaad_' + self.split + '_scenes_2k30_eyes.txt', "r")
+		self.transform = transform
+		self.X, self.Y = self.preprocess()
+
+	def __len__(self):
+		return len(self.Y)
+
+	def get_data(self):
+		return self.X, torch.Tensor(self.Y)
+
+	def __getitem__(self, idx):
+		if torch.is_tensor(idx):
+			idx = idx.tolist()
+		label = self.Y[idx]
+		label = torch.Tensor([label])
+		sample = {'image': Image.open(self.path+self.path_jaad+self.X[idx]), 'label':label}
+		if self.transform:
+			sample['image'] = torch.flatten(self.transform(sample['image']))
+			print(len(sample['image')])
+		return sample['image'], sample['label']
+
+	def evaluate(self, model, device, it):
+		assert self.split in ["test", "val"]
+		model.eval()
+		print("Starting evalutation ..")
+		tab_X, tab_Y = self.X, self.Y
+		idx_Y1 = np.where(np.array(tab_Y) == 1)[0]
+		idx_Y0 = np.where(np.array(tab_Y) == 0)[0]
+		positive_samples = np.array(tab_X)[idx_Y1]
+		positive_samples_labels = np.array(tab_Y)[idx_Y1]
+		N_pos = len(idx_Y1)
+
+		aps = []
+		accs = []
+		for i in range(it):
+			np.random.seed(i)
+			np.random.shuffle(idx_Y0)
+			neg_samples = np.array(tab_X)[idx_Y0[:N_pos]]
+			neg_samples_labels = np.array(tab_Y)[idx_Y0[:N_pos]]
+
+			total_samples = np.concatenate((positive_samples, neg_samples)).tolist()
+			total_labels = np.concatenate((positive_samples_labels, neg_samples_labels)).tolist()
+			new_data = new_Dataset(self.path, self.path_jaad, total_samples, total_labels, self.transform)
+			data_loader = torch.utils.data.DataLoader(new_data, batch_size=16, shuffle=True)
+
+			acc = 0
+			out_lab = torch.Tensor([]).type(torch.float)
+			test_lab = torch.Tensor([])
+			for x_test, y_test in data_loader:
+				x_test, y_test = x_test.to(device), y_test.to(device)
+				output = model(x_test)
+				out_pred = output
+				pred_label = torch.round(out_pred)
+
+				le = x_test.shape[0]
+				acc += le*binary_acc(pred_label.type(torch.float).view(-1), y_test).item()
+				test_lab = torch.cat((test_lab.detach().cpu(), y_test.view(-1).detach().cpu()), dim=0)
+				out_lab = torch.cat((out_lab.detach().cpu(), out_pred.view(-1).detach().cpu()), dim=0)
+
+
+			acc = sum(torch.round(out_lab).to(device) == test_lab.to(device))/len(new_data)
+			ap = average_precision(out_lab, test_lab)
+			accs.append(acc.item())
+			aps.append(ap)
+		return np.mean(aps), np.mean(accs)
+
+	def preprocess(self):
+		"""
+		A helper function to load the paths of the images from the txt file.
+		"""
+		tab_X = []
+		tab_Y = []
+		for line in self.txt:
+			line = line[:-1]
+			line_s = line.split(",")
+
+			tab_X.append(line_s[-2])
+			tab_Y.append(int(line_s[-1]))
+		return tab_X, tab_Y
+
+
+
+
+class new_Dataset(Dataset):
+	"""JAAD dataset for training and inference"""
+
+	def __init__(self, path, path_jaad, data_x, data_y, transform=None):
+		"""
+		Args:
+			split : train, val and test
+			type_ : type of dataset splitting (original splitting, video splitting, pedestrian splitting)
+			transform : data tranformation to be applied
+		"""
+		self.data = None
+		self.path = path
+		self.path_jaad = path_jaad
+		self.transform = transform
+		self.data_x = data_x
+		self.data_y = data_y
+
+	def __len__(self):
+		return len(self.data_y)
+
+	def __getitem__(self, idx):
+		if torch.is_tensor(idx):
+			idx = idx.tolist()
+		label = self.data_y[idx]
+		label = torch.Tensor([label])
+		sample = {'image': Image.open(self.path+self.path_jaad+self.data_x[idx]), 'label': label}
+		if self.transform:
+			sample['image'] = self.transform(sample['image'])
+		return sample['image'], sample['label']
+
+	def preprocess(self):
+		"""
+		A helper function to load the paths of the images from the txt file.
+		"""
+		tab_X = []
+		tab_Y = []
+		for line in self.txt:
+			line = line[:-1]
+			line_s = line.split(",")
+			tab_X.append(line_s[-2])
+			tab_Y.append(int(line_s[-1]))
+		return tab_X, tab_Y
---
+++ looking/eyes/main.py
@@ -0,0 +1,118 @@
+from dataset import *
+from utils import *
+import torch
+import torch.nn as nn
+from torchvision import transforms, datasets
+from torchvision import datasets, models, transforms
+import torchvision.transforms.functional as F
+import argparse
+
+torch.manual_seed(0)
+
+# Parser
+
+parser = argparse.ArgumentParser(description='Training the eyes model on JAAD')
+
+# parameters
+
+parser.add_argument('--epochs', '-e', type=int, help='number of epochs for training', default=100)
+parser.add_argument('--learning_rate', '-lr', type=float, help='learning rate for training', default=0.0001)
+parser.add_argument('--path', type=str, help='path for model saving', default='./models/')
+parser.add_argument('--jaad_split_path', type=str, help='proportion for the training', default="JAAD_2k30_eyes/")
+parser.add_argument('--split_path', '-jsp', type=str, help='proportion for the training', default="/home/caristan/looking/looking/splits/")
+parser.add_argument('--data_path', '-dp', type=str, help='proportion for the training', default="/home/caristan/code/looking/looking/data/")
+
+
+
+
+EPOCHS = args.epochs
+
+
+DATA_PATH = args.data_path
+SPLIT_PATH = args.split_path
+JAAD_PATH = args.jaad_split_path
+PATH_MODEL = args.path
+
+
+"""
+My local paths
+DATA_PATH = '../../data/'
+SPLIT_PATH_JAAD = '../splits/'
+PATH_MODEL = './models/'
+"""
+
+# Resize
+resize = (10, 15)
+
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+print('Device: ', device)
+
+data_transform = transforms.Compose([
+	SquarePad(),
+    transforms.ToTensor(),
+	transforms.ToPILImage(),
+    transforms.Resize(resize),
+	transforms.ToTensor(),
+])
+
+
+INPUT_SIZE = 450
+
+model = LookingModel(INPUT_SIZE).to(device)
+
+jaad_train = JAAD_Dataset(DATA_PATH, JAAD_PATH, "train", SPLIT_PATH, data_transform)
+jaad_val = JAAD_Dataset(DATA_PATH, JAAD_PATH, "val", SPLIT_PATH, data_transform)
+
+dataset_loader = torch.utils.data.DataLoader(jaad_train, batch_size=64, shuffle=True)
+dataset_loader_test = torch.utils.data.DataLoader(jaad_val, batch_size=8, shuffle=True)
+
+best_ap, best_ac, best_epoch = 0, 0, 0
+
+
+criterion = nn.BCELoss()
+test_ap = []
+test_ac = []
+optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+model.train()
+for epoch in range(EPOCHS):
+	i = 0
+	model.to(device)
+	running_loss = 0
+	for x_batch, y_batch in dataset_loader:
+		if torch.cuda.is_available():
+			x_batch, y_batch = x_batch.to(device), y_batch.to(device).float()
+		optimizer.zero_grad()
+		output = model(x_batch.float())
+		loss = criterion(output, y_batch.view(-1, 1))
+		running_loss += loss.item() * x_batch.size(0)
+		loss.backward()
+		optimizer.step()
+		i+=1
+		if i%10 == 0:
+			model.eval()
+			out_pred = output
+			pred_label = torch.round(out_pred)
+			acc = binary_acc(pred_label.type(torch.float).view(-1), y_batch).item()
+			print('step {} , loss :{} | acc:{} '.format(i, loss.item(), acc))
+			model.train()
+			#break
+	model.eval()
+	torch.cuda.empty_cache()
+	model.to("cpu")
+
+	ap_test, acc_test = jaad_val.evaluate(model, device)
+
+	test_ap.append(ap_test)
+	test_ac.append(acc_test)
+	#print(acc0, acc1)
+
+	print_summary(epoch+1, EPOCHS, acc_test, ap_test)
+
+	model.train()
+	if ap_test > best_ap:
+		best_epoch = epoch
+		best_ap = ap_test
+		best_ac = acc_test
+		print(f'Best epoch: {best_epoch}, AP: {best_ap}, Acc: {best_acc}')
+		torch.save(model, PATH_MODEL + 'eyes_model.pkl'))
